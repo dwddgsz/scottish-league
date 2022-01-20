6fafc7e7279bdb57554d7af38851aee87cbc03ec
dwddgsz <dwddgsz@gmail.com>
2022-01-20 16:08:49 +0100

    add: rendering

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navigation from './layouts/Navigation';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <>
+        <Navigation/>
+        <Home/>
+    </>
+  );
+};
+
+export default App;
