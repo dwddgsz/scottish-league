d8c49a25caf452d86a53ccc4a893c6b93959242e
dwddgsz <dwddgsz@gmail.com>
2022-01-20 16:08:33 +0100

    add: LatestMatches component bare bones

---
+++ src/components/LatestMatches.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const LatestMatches = () => {
+  return (
+    <LatestMatchesWrapper>
+        s
+    </LatestMatchesWrapper>
+    );
+};
+
+const LatestMatchesWrapper = styled.ul`
+    padding:15px;
+    border: 2px solid var(--border);
+    border-radius:3px;
+`
+
+export default LatestMatches;
