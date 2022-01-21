99b55f1fca3db10a8a20c61d39386a50af8175d3
dwddgsz <dwddgsz@gmail.com>
2022-01-21 12:04:41 +0100

    remove: Title padding-left

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const Title:React.FC = ({children}) => (<TitleWrapper>{children}</TitleWrapper>);
 
 const TitleWrapper = styled.h2`
-    padding-left:15px;
     font-size: 2rem;
     font-weight: 500;
     color: var(--title);
