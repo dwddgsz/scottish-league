6c6d1ee656b3f65bd5370db156ff068824b1f507
dwddgsz <dwddgsz@gmail.com>
2022-01-24 13:58:19 +0100

    add: components rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import Navigation from './layouts/Navigation';
 import Home from './pages/Home';
+import MatchDetails from './pages/MatchDetails';
+import TopScorers from './pages/TopScorers'
+import NotFound from './pages/NotFound'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
@@ -7,8 +10,10 @@ const App = () => {
     <Router>
         <Navigation/>
         <Routes>
+          <Route path="*" element={<NotFound/>}></Route>
+          <Route path="/top-scorers" element={<TopScorers/>}></Route>
           <Route path="/" element={<Home/>}></Route>
-          <Route path="/match/:id" element={<div>match</div>}></Route>
+          <Route path="/match/:id" element={<MatchDetails/>}></Route>
         </Routes>
     </Router>
   );
