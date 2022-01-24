6fb6bd7525fd0a988a4a800e3b93514d5e1883c7
dwddgsz <dwddgsz@gmail.com>
2022-01-24 15:42:06 +0100

    format: with prettier

--- src/components/LatestMatchElement.tsx
+++ src/components/LatestMatchElement.tsx
@@ -1,133 +1,147 @@
-import {latestMatch} from './LatestMatches';
-import { useNavigate } from 'react-router-dom';
+import { latestMatch } from "./LatestMatches";
+import { useNavigate } from "react-router-dom";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
-const LatestMatchElement:React.FC<{match:latestMatch}> = ({match}) => {
-    const {id,localTeam,visitorTeam} = match;
-    const {date} = match.time.starting_at;
-    const {ht_score,ft_score,localteam_score,visitorteam_score} = match.scores;
+const LatestMatchElement: React.FC<{ match: latestMatch }> = ({ match }) => {
+  const { id, localTeam, visitorTeam } = match;
+  const { date } = match.time.starting_at;
+  const { ht_score, ft_score, localteam_score, visitorteam_score } =
+    match.scores;
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const navigateToDetails = () => {
-        navigate(`/match/${id}`);
-    }
+  const navigateToDetails = () => {
+    navigate(`/match/${id}`);
+  };
 
   return (
     <LatestMatchElementWrapper>
-
-        <span className="match__date">{date}</span>
-        <div className="match__info">
+      <span className="match__date">{date}</span>
+      <div className="match__info">
         <figure className="match__team match__team">
-            <figcaption className={localteam_score > visitorteam_score ? 'winner' : ''}>{localTeam.data.name}</figcaption>
-            <img className="match__logo  match__logo--home" src={localTeam.data.logo_path} alt="team-logo"></img>
+          <figcaption
+            className={localteam_score > visitorteam_score ? "winner" : ""}
+          >
+            {localTeam.data.name}
+          </figcaption>
+          <img
+            className="match__logo  match__logo--home"
+            src={localTeam.data.logo_path}
+            alt="team-logo"
+          ></img>
         </figure>
 
         <p className="match__score">
-            <span className="match__full-time">{ft_score}</span>
-            <span className="match__half-time">{ht_score}</span>
+          <span className="match__full-time">{ft_score}</span>
+          <span className="match__half-time">{ht_score}</span>
         </p>
 
         <figure className="match__team match__team--away">
-            <img className="match__logo match__logo--away" src={visitorTeam.data.logo_path} alt="team-logo"></img>
-            <figcaption className={localteam_score < visitorteam_score ? 'winner' : ''}>{visitorTeam.data.name}</figcaption>
+          <img
+            className="match__logo match__logo--away"
+            src={visitorTeam.data.logo_path}
+            alt="team-logo"
+          ></img>
+          <figcaption
+            className={localteam_score < visitorteam_score ? "winner" : ""}
+          >
+            {visitorTeam.data.name}
+          </figcaption>
         </figure>
-        
-        </div>
+      </div>
 
-        <button className="match__more" onClick={navigateToDetails}>
-            More
-        </button>
+      <button className="match__more" onClick={navigateToDetails}>
+        More
+      </button>
     </LatestMatchElementWrapper>
-    );
+  );
 };
 
 const LatestMatchElementWrapper = styled.li`
-    display:flex;
-    align-items:center;
-    padding:13px 15px;
-    border-bottom: 2px solid var(--border);
-    color: var(--title);
-    @media only screen and (min-width:500px) {
-        font-size:1.25rem;
+  display: flex;
+  align-items: center;
+  padding: 13px 15px;
+  border-bottom: 2px solid var(--border);
+  color: var(--title);
+  @media only screen and (min-width: 500px) {
+    font-size: 1.25rem;
+  }
+  .match {
+    &__date {
+      @media only screen and (min-width: 600px) {
+        width: 90px;
+      }
     }
-    .match {
-        &__date {
-            @media only screen and (min-width:600px) {
-                width:90px;
-            }
-        }
-        &__info {
-            display:flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            width:100%;
-            @media only screen and (min-width:600px) {
-                flex-direction: row;
-            }
-        }
-        &__team {
-            display:flex;
-            align-items: center;
-            @media only screen and (min-width:600px) {
-                width:150px;
-                justify-content: flex-end;
-            }
-            &--away {
-                    @media only screen and (min-width:600px) {
-                    justify-content: flex-start;
-                }
-            }
-            .winner {
-                font-weight: bold;
-            }
-        }
-        &__logo {
-            width:45px;
-            height:45px;
-            display:none;
-            @media only screen and (min-width:600px) {
-                display:flex;
-            }
-            &--home {
-                margin-left:10px;
-            }
-            &--away {
-                margin-right:10px;
-            }
-        }  
-        &__score {
-            width:25px;
-            margin: 0 20px;
-            display:flex;
-            flex-direction: column;
-            text-align: center;
-            background-color: var(--border);
-        }          
-        &__full-time {
-                font-weight:bold;
-                color: var(--title);
-            }
-        &__half-time {
-            font-size:0.9rem;
-        }
-        &__more {
-            margin-left:auto;
-            padding: 3px 8px;
-            border:none;
-            font-size:1.3rem;
-            background-color: var(--border);
-            color: var(--title);
-            cursor:pointer;
-            &:hover {
-                background-color: var(--title);
-                color: var(--border);
-            }
-        }
+    &__info {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      @media only screen and (min-width: 600px) {
+        flex-direction: row;
+      }
     }
-`
-
+    &__team {
+      display: flex;
+      align-items: center;
+      @media only screen and (min-width: 600px) {
+        width: 150px;
+        justify-content: flex-end;
+      }
+      &--away {
+        @media only screen and (min-width: 600px) {
+          justify-content: flex-start;
+        }
+      }
+      .winner {
+        font-weight: bold;
+      }
+    }
+    &__logo {
+      width: 45px;
+      height: 45px;
+      display: none;
+      @media only screen and (min-width: 600px) {
+        display: flex;
+      }
+      &--home {
+        margin-left: 10px;
+      }
+      &--away {
+        margin-right: 10px;
+      }
+    }
+    &__score {
+      width: 25px;
+      margin: 0 20px;
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+      background-color: var(--border);
+    }
+    &__full-time {
+      font-weight: bold;
+      color: var(--title);
+    }
+    &__half-time {
+      font-size: 0.9rem;
+    }
+    &__more {
+      margin-left: auto;
+      padding: 3px 8px;
+      border: none;
+      font-size: 1.3rem;
+      background-color: var(--border);
+      color: var(--title);
+      cursor: pointer;
+      &:hover {
+        background-color: var(--title);
+        color: var(--border);
+      }
+    }
+  }
+`;
 
 export default LatestMatchElement;
--- src/components/TableElement.tsx
+++ src/components/TableElement.tsx
@@ -1,71 +1,77 @@
-import styled from 'styled-components';
-import {standing} from './Table';
+import styled from "styled-components";
+import { standing } from "./Table";
 
 interface TableElementWrapperProps {
-    header?:boolean;
+  header?: boolean;
 }
 
 interface TableElementProps {
-    header?:boolean;
-    standing:standing;
+  header?: boolean;
+  standing: standing;
 }
 
-
-const TableElement:React.FC<TableElementProps> = ({header,standing}) => {
-    const {team_name} = standing;
-    const {games_played,won,draw,lost,goals_scored,goals_against,points} = standing.overall;
+const TableElement: React.FC<TableElementProps> = ({ header, standing }) => {
+  const { team_name } = standing;
+  const { games_played, won, draw, lost, goals_scored, goals_against, points } =
+    standing.overall;
   return (
-        <TableElementWrapper header={header}>
-            <h4 className="team-stats__name">{team_name}</h4>
-            <span className="team-stats__played">{games_played}</span>
-            <span className="team-stats__wins">{won}</span>
-            <span className="team-stats__draws">{draw}</span>
-            <span className="team-stats__losses">{lost}</span>
-            <span className="team-stats__scored">{goals_scored}</span>
-            <span className="team-stats__conceded">{goals_against}</span>
-            <span className="team-stats__points">{points}</span>
-        </TableElementWrapper>
-    );
+    <TableElementWrapper header={header}>
+      <h4 className="team-stats__name">{team_name}</h4>
+      <span className="team-stats__played">{games_played}</span>
+      <span className="team-stats__wins">{won}</span>
+      <span className="team-stats__draws">{draw}</span>
+      <span className="team-stats__losses">{lost}</span>
+      <span className="team-stats__scored">{goals_scored}</span>
+      <span className="team-stats__conceded">{goals_against}</span>
+      <span className="team-stats__points">{points}</span>
+    </TableElementWrapper>
+  );
 };
 
 export const TableElementWrapper = styled.li<TableElementWrapperProps>`
-    display:flex;
-    color: var(--title);
-    padding: 5px 15px;
-    &:nth-child(even) {
-        background-color: var(--bgc);
+  display: flex;
+  color: var(--title);
+  padding: 5px 15px;
+  &:nth-child(even) {
+    background-color: var(--bgc);
+  }
+  span {
+    text-align: center;
+    font-weight: ${(props) => (props.header ? "500" : "")};
+    @media only screen and (min-width: 500px) {
+      font-size: 1.4rem;
     }
-    span {
-        text-align:center;
-        font-weight: ${(props) => props.header ? '500':''};
-        @media only screen and (min-width:500px) {
-                font-size:1.4rem;
-            }
+  }
+  border-bottom: ${(props) =>
+    props.header ? "2px solid var(--border)" : "none"};
+  .team-stats {
+    &__name {
+      font-size: 1.2rem;
+      font-weight: 400;
+      width: 50%;
+      font-weight: ${(props) => (props.header ? "500" : "")};
+      @media only screen and (min-width: 500px) {
+        width: 30%;
+        font-size: 1.4rem;
+      }
     }
-    border-bottom: ${(props) => props.header ? '2px solid var(--border)' :'none'};  
-    .team-stats {
-        &__name {
-            font-size:1.2rem;
-            font-weight:400;
-            width:50%;
-            font-weight: ${(props) => props.header ? '500':''};
-            @media only screen and (min-width:500px) {
-                width:30%;
-                font-size:1.4rem;
-            }
-        }
-        &__played,&__wins,&__draws,&__losses,&__points {
-            width:10%;
-        }
+    &__played,
+    &__wins,
+    &__draws,
+    &__losses,
+    &__points {
+      width: 10%;
+    }
 
-        &__scored,&__conceded{
-            display:none;
-            width:10%;
-            @media only screen and (min-width:500px) {
-                display:block;
-            }
-        }
+    &__scored,
+    &__conceded {
+      display: none;
+      width: 10%;
+      @media only screen and (min-width: 500px) {
+        display: block;
+      }
     }
-`
+  }
+`;
 
 export default TableElement;
