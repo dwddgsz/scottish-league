fc4ea2cf80d50242ad5b518935526487c1f18929
dwddgsz <dwddgsz@gmail.com>
2022-01-24 13:56:49 +0100

    change: font linking method

--- src/GlobalStyle/GlobalStyle.js
+++ src/GlobalStyle/GlobalStyle.js
@@ -1,8 +1,6 @@
 import {createGlobalStyle} from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
 
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap');
-
 :root {
   --white: #fff;
   --border: #F3F5F9;
